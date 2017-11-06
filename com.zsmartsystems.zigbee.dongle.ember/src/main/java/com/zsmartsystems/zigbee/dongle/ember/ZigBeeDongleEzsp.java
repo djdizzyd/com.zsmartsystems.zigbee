223d764d88a83fd25dab52817d14765d9df23268
cschwer <charles.schwer@gmail.com>
2017-11-06 16:28:38 -0600

    add OTA cluster to list of endpoints

--- com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ZigBeeDongleEzsp.java
+++ com.zsmartsystems.zigbee.dongle.ember/src/main/java/com/zsmartsystems/zigbee/dongle/ember/ZigBeeDongleEzsp.java
@@ -358,7 +358,8 @@ public class ZigBeeDongleEzsp implements ZigBeeTransportTransmit, EzspFrameHandl
     private void createEndpoints() {
         // Create a list of all the clusters we want to register
         final List<Integer> clusterSet = Arrays.asList(ZclClusterType.BASIC.getId(),
-                ZclClusterType.POWER_CONFIGURATION.getId(), ZclClusterType.ON_OFF.getId(), ZclClusterType.IAS_ZONE.getId());
+                ZclClusterType.POWER_CONFIGURATION.getId(), ZclClusterType.ON_OFF.getId(), ZclClusterType.IAS_ZONE.getId(),
+                0x0019);
 
         EzspAddEndpointRequest addEndpoint;
 
